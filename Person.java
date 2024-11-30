afcb8df9e1275bf080636b4a2a2282f015d94277
rameshramesh123 <153923182+rameshramesh123@users.noreply.github.com>
2024-11-30 18:58:18 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,6 +1,23 @@
-package Solution_1_4;
+package solution1_1;
 
-public class Person {
+ class Person {
+    int age=18;
     String name;
-    int age;
+
+    public void displayDetails(){
+        System.out.println("Person age is"+" "+this.age);
+        System.out.println("Person name is"+" "+name);
+    }
+    public Person(int age,String name){
+        this.age=age;
+        this.name=name;
+
+    }
+     public static void main(String[] args) {
+         Person person=new Person(18,"Ramesh");
+         person.displayDetails();
+
+
+     }
+
 }
